5ed7c77ce78638430f3dd0bee4659f5871396362
Luis Felipe Mileo <mileo@kmee.com.br>
2019-04-22 11:40:47 -0300

    [NEW] Teste de assinatura de pdf
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- tests/test_edoc_assinatura.py
+++ tests/test_edoc_assinatura.py
@@ -1,6 +1,50 @@
+# coding=utf-8
+
+import os
+from datetime import datetime
+from tzlocal import get_localzone
 
 from edoc_assinatura.cli import main
+from edoc_assinatura.certificado import Certificado
+from edoc_assinatura.assinatura import Assinatura
+
+caminho = os.environ['caminho_certificado']
+senha = os.environ['senha_certificado']
 
 
 def test_main():
     assert main([]) == 0
+
+
+def test_chave_cert():
+    d = Certificado(caminho, senha)
+    chave, certificado = d.separa_chave_certificado(senha)
+    assert chave, certificado
+
+
+def test_assinatura_pdf():
+    certificado = Certificado(caminho, senha)
+    assinador = Assinatura(certificado)
+
+    nome_arquivo = 'tests/google.pdf'
+    arquivo = open(nome_arquivo, 'rb').read()
+
+    dados_assinatura = {
+        b'sigflags': 3,
+        b'contact': b'KMEE INFORMATICA LTDA',
+        b'location': b'BR',
+        b'signingdate': str.encode(
+            datetime.now(get_localzone()).strftime("%Y%M%d%H%M%S%Z")),
+        b'reason': b'Teste edoc assinatura',
+    }
+
+    assinatura = assinador.assina_pdf(
+        arquivo=arquivo,
+        dados_assinatura=dados_assinatura,
+    )
+
+    nome_arquivo = nome_arquivo.replace('.pdf', '-signed-cms.pdf')
+    with open(nome_arquivo, 'wb') as fp:
+        fp.write(arquivo)
+        fp.write(assinatura)
+
