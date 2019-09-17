301ad11de280ad893472330d3631d9e400f0fab2
Luis Felipe Mileo <mileo@kmee.com.br>
2019-09-17 17:15:09 -0300

    Bump version: 0.0.8 → 0.0.9

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def read(*names, **kwargs):
 
 setup(
     name='erpbrasil.assinatura',
-    version='0.0.8',
+    version='0.0.9',
     license='MIT license',
     description='Assinatura de documentos com certificados digitais A1 e A3',
     long_description='%s\n%s' % (
