c7161017541b2ef2cce236a7d9fb8c1bca55cd53
Luis Felipe Mileo <mileo@kmee.com.br>
2019-09-17 11:23:42 -0300

    [FIX] signxml dependencie

--- setup.py
+++ setup.py
@@ -73,6 +73,7 @@ setup(
     ],
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
     install_requires=[
+        'signxml',
         'cryptography',
         'endesive',
         'tzlocal',
