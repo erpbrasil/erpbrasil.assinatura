7a1715f0faaf721741c54fe233c54e472e24960c
Luis Felipe Mileo <mileo@kmee.com.br>
2019-09-23 07:56:03 -0300

    [NEW] Namespace Packages
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- setup.py
+++ setup.py
@@ -50,6 +50,7 @@ setup(
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
+    namespace_packages=["erpbrasil"],
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
--- src/erpbrasil/assinatura/__init__.py
+++ src/erpbrasil/assinatura/__init__.py
@@ -1,1 +1,5 @@
 __version__ = '0.1.0'
+
+from erpbrasil.assinatura.assinatura import Assinatura
+from erpbrasil.assinatura.certificado import Certificado
+
