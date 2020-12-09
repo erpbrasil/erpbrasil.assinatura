fab67f9021788ce785c023d39b6f649c363e1fda
Luis Felipe Mileo <mileo@kmee.com.br>
2020-12-09 13:33:11 -0300

    Bump version: 1.0.1 → 1.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'erpbrasil.assinatura'
 year = '2019'
 author = 'Luis Felipe Mileo'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '1.0.1'
+version = release = '1.0.2'
 
 pygments_style = 'trac'
 templates_path = ['.']
--- src/erpbrasil/assinatura/__init__.py
+++ src/erpbrasil/assinatura/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 from erpbrasil.assinatura.assinatura import Assinatura  # noqa: F401
 from erpbrasil.assinatura.certificado import Certificado  # noqa: F401
