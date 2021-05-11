8769407160d4dde8250628605f38d2e0a8b99f67
Ygor Carvalho <ygcarvalh@gmail.com>
2021-05-11 14:11:56 -0300

    [FIX] Functions are now methods of the class

--- src/erpbrasil/assinatura/assinatura.py
+++ src/erpbrasil/assinatura/assinatura.py
@@ -1,17 +1,13 @@
 # coding=utf-8
-
-from base64 import b64encode
-
 import signxml
-import xmlsec
+from OpenSSL import crypto
+from base64 import b64encode
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
 from endesive import pdf
 from endesive import signer
 from endesive import xades
 from lxml import etree
-from OpenSSL import crypto
-from xmlsec import constants as consts
 
 
 class Assinatura(object):
@@ -74,11 +70,6 @@ class Assinatura(object):
         if not self.chave_privada:
             raise Exception("Certificado não existe.")
 
-
-
-
-
-
     def assina_pdf(self, arquivo, dados_assinatura, altoritimo='sha256'):
         return pdf.cms.sign(
             datau=arquivo,
@@ -94,29 +85,6 @@ class Assinatura(object):
         return pdf.verify(
             data=arquivo,
             trusted_cert_pems=certificados_de_confianca
-        )
-
-    def assina_xml(self, arquivo):
-        def signproc(tosign, algosig):
-            key = self.certificado.key
-            signed_value_signature = key.sign(
-                tosign,
-                padding.PKCS1v15(),
-                getattr(hashes, algosig.upper())()
-            )
-            return signed_value_signature
-
-        cert = self.certificado.cert
-        certcontent = signer.cert2asn(cert).dump()
-
-        cls = xades.BES()
-        doc = cls.enveloping(
-            'documento.xml', arquivo, 'application/xml',
-            cert, certcontent, signproc, False, True
-        )
-
-        return etree.tostring(
-            doc, encoding='UTF-8', xml_declaration=True, standalone=False
         )
 
     def assina_string(self, message):
