5ea0b0192d95ca858ced4f64083ddbb7908b82f7
Luis Felipe Mileo <mileo@kmee.com.br>
2021-05-26 12:21:47 -0300

    Bump version: 1.1.0 → 1.2.0

--- src/erpbrasil/assinatura/__init__.py
+++ src/erpbrasil/assinatura/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.0'
+__version__ = '1.2.0'
 
 from erpbrasil.assinatura.assinatura import Assinatura  # noqa: F401
 from erpbrasil.assinatura.certificado import Certificado  # noqa: F401
