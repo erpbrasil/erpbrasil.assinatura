27d87cfd7e788ac6fac92482cb6ee91a398ff5f2
Marcel Savegnago <marcel.savegnago@gmail.com>
2023-01-24 15:05:01 -0300

    [FIX] Remove namespace prefix from Signature tags

--- src/erpbrasil/assinatura/assinatura.py
+++ src/erpbrasil/assinatura/assinatura.py
@@ -55,7 +55,7 @@ class Assinatura(object):
             c14n_algorithm='http://www.w3.org/TR/2001/REC-xml-c14n-20010315'
         )
 
-        signer.namespaces = {"ds": "http://www.w3.org/2000/09/xmldsig#"}
+        signer.namespaces = {None: "http://www.w3.org/2000/09/xmldsig#"}
 
         ref_uri = ('#%s' % reference) if reference else None
 
